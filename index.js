6a39dffd9e340ffad20a60922414b45a2233c023
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-05-26 17:24:43 +0200

    Conversion de Snake a Camel

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let palabraSnake = "hola-como-estas";
+let palabraCamel = "holaComoEstas";
 
+let palabraFinalSnake;
+
+palabraSnake = palabraSnake.split("-");
+
+arrayPalabrasToCamel(palabraSnake);
+
+function arrayPalabrasToCamel(arrayPalabras) {
+  let palabraFinalCamel;
+
+  arrayPalabras.forEach((element) => {
+    if (typeof palabraFinalCamel !== "undefined") {
+      palabraFinalCamel += element[0].toUpperCase() + element.substring(1);
+    } else {
+      palabraFinalCamel = element[0].toUpperCase() + element.substring(1);
+    }
+  });
+
+  console.log(palabraFinalCamel);
+}
